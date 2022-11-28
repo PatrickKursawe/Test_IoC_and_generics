7b3f615e4d219b1cbe166856475f73e413682733
Patrick Kursawe <kursawe@axelsemrau.de>
2022-11-28 16:09:54 +0100

    Updated with infrastructure usage in plugin class constructor.

--- Class1.cs
+++ Class1.cs
@@ -13,9 +13,22 @@ namespace Test_IoC_and_generics
 
     }
 
-    public class Plugin : IPlugin
+    public interface IInfrastructure
     {
 
+    }
+
+    public class Infrastructure : IInfrastructure
+    {
+
+    }
+
+    public class Plugin : IPlugin
+    {
+        public Plugin(IInfrastructure _)
+        {
+
+        }
     }
 
     public class PluginWrapper<TImpl> where TImpl: class, IPlugin
@@ -29,6 +42,7 @@ namespace Test_IoC_and_generics
     {
         public Investigation()
         {
+            ServiceLocator.Default.RegisterType<IInfrastructure, Infrastructure>(RegistrationType.Transient);
             ServiceLocator.Default.RegisterType(typeof(Plugin),typeof(Plugin), RegistrationType.Transient);
             var wrapperType = typeof(PluginWrapper<>).MakeGenericType(typeof(Plugin));
             ServiceLocator.Default.RegisterType(wrapperType, wrapperType,RegistrationType.Transient);
