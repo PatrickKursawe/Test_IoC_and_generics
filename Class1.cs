25e6b5ec71e257edc08fd7d16955da21519f1941
Patrick Kursawe <kursawe@axelsemrau.de>
2022-11-28 15:57:17 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catel.IoC;
+using Xunit;
+
+namespace Test_IoC_and_generics
+{
+    public interface IPlugin
+    {
+
+    }
+
+    public class Plugin : IPlugin
+    {
+
+    }
+
+    public class PluginWrapper<TImpl> where TImpl: class, IPlugin
+    {
+        public PluginWrapper(TImpl _)
+        {
+        }
+    }
+
+    public class Investigation
+    {
+        public Investigation()
+        {
+            ServiceLocator.Default.RegisterType(typeof(Plugin),typeof(Plugin), RegistrationType.Transient);
+            var wrapperType = typeof(PluginWrapper<>).MakeGenericType(typeof(Plugin));
+            ServiceLocator.Default.RegisterType(wrapperType, wrapperType,RegistrationType.Transient);
+        }
+
+        [Fact]
+        public void CanResolvePlugin()
+        {
+            Assert.NotNull(ServiceLocator.Default.ResolveType<Plugin>());
+        }
+
+        [Fact]
+        public void CanResolveWrapper()
+        {
+            Assert.NotNull(ServiceLocator.Default.ResolveType<PluginWrapper<Plugin>>());
+        }
+    }
+}
